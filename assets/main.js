6bf652f422a0c17f8e0a7dba8a1a745c2658b020
FrancescoDale <fd060862@gmail.com>
2020-10-14 14:55:24 +0200

    creato ciclo for

--- assets/main.js
+++ assets/main.js
@@ -3,3 +3,9 @@ var mailList = ['xyz@gmail.com', 'asd@gmail.com', 'qwerty@gmail.com' ];
 var userMail;
 
 userMail = prompt(' inserire qui la vostra email : ');
+
+// ciclo for che verifica se la lunghezza della mail inserita è compatibile con una nell'array
+
+for (var i = 0; i < mailList.length; i++) {
+
+}
