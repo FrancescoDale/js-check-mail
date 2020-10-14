25633b1a6bcec4800b7e8a413cfdb83260c0213d
FrancescoDale <fd060862@gmail.com>
2020-10-14 15:05:32 +0200

    assegnati valori alla variabile e messo un if/else per visualizzare il risultato sull'index

--- assets/main.js
+++ assets/main.js
@@ -8,5 +8,14 @@ userMail = prompt(' inserire qui la vostra email : ');
 // ciclo for che verifica se la lunghezza della mail inserita è compatibile con una nell'array
 
 for (var i = 0; i < mailList.length; i++) {
-    x = 1;
+    if ( userMail == mailList[i] ) {
+        x = 1;
+    }
+}
+
+if (x = 1) {
+    document.getElementById('chkmail').innerHTML = ' mail presente';
+     }
+else {
+    document.getElementById('chkmail').innerHTML = ' spiacenti, mail non presente';
 }
