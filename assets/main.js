b0d488704ee6fcd18dd4bc7a1034b3ec11fec9c0
FrancescoDale <fd060862@gmail.com>
2020-10-14 14:40:41 +0200

    fix array e prompt

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,5 @@
-var mailList ['xyz@gmail.com', 'asd@gmail.com', 'qwerty@gmail.com' ];
+var mailList = ['xyz@gmail.com', 'asd@gmail.com', 'qwerty@gmail.com' ];
+
+var userMail;
+
+userMail = prompt(' inserire qui la vostra email : ');
